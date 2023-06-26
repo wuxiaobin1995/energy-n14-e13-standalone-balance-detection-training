13705c519307db2f904050b41ca7813c7467df28
wuxiaobin1995 <845225263@qq.com>
2023-06-26 16:06:20 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author      : Mr.bin
  * @Date        : 2021-09-13 16:45:54
- * @LastEditTime: 2023-03-02 11:54:06
+ * @LastEditTime: 2023-06-26 10:44:30
  * @Description : 根组件
 -->
 <template>
@@ -15,13 +15,30 @@ export default {
   name: 'App',
 
   created() {
-    /* 初始化边界圆半径值 */
-    if (!window.localStorage.getItem('boundaryAngle')) {
-      window.localStorage.setItem('boundaryAngle', 12)
-    }
-    /* 初始化医院名称 */
-    if (!window.localStorage.getItem('hospital')) {
-      window.localStorage.setItem('hospital', '')
+    this.initLocalStorage()
+  },
+
+  methods: {
+    /**
+     * @description: 软件启动后，初始化localStorage的一些值
+     */
+    initLocalStorage() {
+      /* 初始化边界圆半径值 */
+      if (!window.localStorage.getItem('boundaryAngle')) {
+        window.localStorage.setItem('boundaryAngle', 12)
+      }
+      /* 初始化医院名称 */
+      if (!window.localStorage.getItem('hospital')) {
+        window.localStorage.setItem('hospital', '')
+      }
+      /* 初始化免责声明的终端用户名称 */
+      if (!window.localStorage.getItem('disclaimer_name')) {
+        window.localStorage.setItem('disclaimer_name', '')
+      }
+      /* 初始化免责声明的设备编号 */
+      if (!window.localStorage.getItem('disclaimer_device_id')) {
+        window.localStorage.setItem('disclaimer_device_id', '')
+      }
     }
   }
 }
